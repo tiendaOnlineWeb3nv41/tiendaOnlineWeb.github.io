05f36c994bfd80f3bb2e0dbcd697cc7f425cd385
tiendaOnlineWeb3nv41 <85773424+tiendaOnlineWeb3nv41@users.noreply.github.com>
2021-06-12 10:35:58 -0500

    Add files via upload

---
+++ js/faros.js
@@ -0,0 +1,9 @@
+
+function comprar(){
+    var modelo=Number(document.getElementById('modelo').value);
+    var cantidad=Number(document.getElementById('cantidad').value);
+    var marca=document.getElementById('');
+    var producto=document.getElementById('nombre');
+    console.log(modelo);
+    console.log(cantidad);
+}
---
+++ js/sign_in.js
@@ -0,0 +1,54 @@
+var loginWithGoogle=function(){
+
+            var provider = new firebase.auth.GoogleAuthProvider();
+
+            firebase.auth()
+            .signInWithPopup(provider)
+            .then((result) => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential;
+
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken;
+                // The signed-in user info.
+                var user = result.user;
+                // ...
+                var enviar=document.getElementById('enviar');
+                enviar.innerHTML=`<a href="inicio_.html">
+                                  <button class="btn_inicia" type="button">Continuar</button>
+                                  </a>`;
+
+            }).catch((error) => {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+}
+
+
+var registraUsuario=function(){
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+}
+  
+
+  if(loginWithGoogle || registraUsuario){
+  
+  }
+  else{
+    console.log(false);
+  }
