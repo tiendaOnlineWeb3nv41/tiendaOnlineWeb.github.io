d73e47ba45691368e21fbfd56b912df2a5c2fc4a
tiendaOnlineWeb3nv41 <85773424+tiendaOnlineWeb3nv41@users.noreply.github.com>
2021-06-12 10:33:34 -0500

    Delete js directory

--- js/faros.js
+++
@@ -1,9 +0,0 @@
-
-function comprar(){
-    var modelo=Number(document.getElementById('modelo').value);
-    var cantidad=Number(document.getElementById('cantidad').value);
-    var marca=document.getElementById('');
-    var producto=document.getElementById('nombre');
-    console.log(modelo);
-    console.log(cantidad);
-}
