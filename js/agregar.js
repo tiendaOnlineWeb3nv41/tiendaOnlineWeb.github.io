65aa850b8b0c8ad16b874b9a5b59f0024634eb1c
tiendaOnlineWeb3nv41 <85773424+tiendaOnlineWeb3nv41@users.noreply.github.com>
2021-06-12 12:16:21 -0500

    Create agregar.js

---
+++ js/agregar.js
@@ -0,0 +1,41 @@
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+function agregarBalatas(){
+    var db = firebase.firestore();
+    var modelo=document.getElementById('modelo').value;
+    var cantidad=Number(document.getElementById('cantidad').value);
+    var marca=document.getElementById('marca').value;
+    var producto=document.getElementById('nombre').value;
+    var precio=document.getElementById('precio').value;
+    //salidas
+    var salidaModelo=document.getElementById('salidaModelo'),
+        salidaMarca=document.getElementById('salidaMarca'),
+        salidaCantidad=document.getElementById('salidaCantidad'),
+        salidaProducto=document.getElementById('salidaProducto'),
+        salidaTotal=document.getElementById('salidaTotal');
+    salidaMarca.innerHTML=marca;
+    salidaCantidad.innerHTML=cantidad;
+    salidaModelo.innerHTML=modelo;
+    salidaProducto.innerHTML=producto;
+    salidaTotal.innerHTML=precio*cantidad+" $";
+
+    //Agregamos a la coleccion de balatas
+    db.collection("balatas").add({
+        Marca: marca,
+        Modelo: modelo,
+        Precio: 0,
+        Cantidad:precio
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+        alert("exito",docRef);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+        alert("fallo",error);
+    });
+}
+
+// Add a second document with a generated ID.
