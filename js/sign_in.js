8e90a10c943c139bd89673cf38f5c9f34ace2326
tiendaOnlineWeb3nv41 <85773424+tiendaOnlineWeb3nv41@users.noreply.github.com>
2021-06-11 23:17:13 -0500

    Update sign_in.js

--- js/sign_in.js
+++ js/sign_in.js
@@ -24,3 +24,18 @@ var loginWithGoogle=function(){
                 // ...
             });
 }
+
+
+var registraUsuario=function(){
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+}
