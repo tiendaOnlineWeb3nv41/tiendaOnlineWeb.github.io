d7a98c18270b54e747df21f0768157dae060aff2
tiendaOnlineWeb3nv41 <85773424+tiendaOnlineWeb3nv41@users.noreply.github.com>
2021-06-11 21:47:58 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAiLo3IJ4nJq9xFXuLqbyTKv2wf4TxOZNI",
+    authDomain: "tienda-online-8efef.firebaseapp.com",
+    projectId: "tienda-online-8efef",
+    storageBucket: "tienda-online-8efef.appspot.com",
+    messagingSenderId: "803951788712",
+    appId: "1:803951788712:web:81f17a77691228f109789d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
